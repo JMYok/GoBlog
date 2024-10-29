9c1e0b4cfb83b1517c0952fe5d0caee5e47d8cea
BobJiang <374123140@qq.com>
2024-10-29 09:48:01 +0800

    更新 sql docker-compose.yml

--- sql/blog_user.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO goblog.blog_user (uid, user_name, passwd, avatar, create_at, update_at) VALUES (1, 'admin', '5c6e69a99f90bb00282001f70cec9f85', ' ', '2022-02-14 22:21:02', '0000-00-00 00:00:00');
